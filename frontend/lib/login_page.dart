1787c3edec2fa0dc637ab369cfad16c5b4e490d7
karmehr-arora <karmehr.arora@gmail.com>
2025-10-12 23:16:28 -0700

    new login page Need to test functionality

--- frontend/lib/login_page.dart
+++ frontend/lib/login_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/dashboard.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
